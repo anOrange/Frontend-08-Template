7500c28a2e9d07a919ba2d7040832eb83b06c36e
潘卓 <zhuo.pan@ly.com>
2019-01-01 08:17:22 +0800

    fix: 打包入口

--- Week 15/jsx/webpack.config.js
+++ Week 15/jsx/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: {
     main: './main.js',
-    animationTest: './animationTest.js'
+    // animationTest: './animationTest.js'
   },
   output: {
     filename: '[name].js',
