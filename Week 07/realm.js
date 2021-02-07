9d6ad97b19ef8dd91453522b1683672520a0659a
zhuo.pan <zhuo.pan@ly.com>
2021-02-08 00:40:15 +0800

    fix: 展示效果

--- Week 07/realm.js
+++ Week 07/realm.js
@@ -6,6 +6,22 @@
  * @LastEditors: zhuo.pan
  */
 
-let aa = Symbol('aa')
-console.log(aa instanceof Symbol, typeof aa)
- 
+// 进制转换
+let aa = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
+function NumberToString(num, jinzhi = 10) {
+  let i = num % jinzhi
+  num = (num / jinzhi) >> 0
+  let list = []
+  while(num) {
+    list.push(i)
+    i = num % jinzhi
+    num = (num / jinzhi) >> 0
+  }
+  list.push(i)
+  list = list.reverse().map(i => aa[i])
+  console.log(list)
+  return list.join('')
+}
+
+let a = NumberToString(198, 16)
+console.log(a)
