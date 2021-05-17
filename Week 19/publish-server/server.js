88ec8073f2c9bd2d89d0f7e6e462a3604bedf211
zhuo.pan <zhuo.pan@ly.com>
2021-05-17 19:42:55 +0800

    chore: WriteStream

--- Week 19/publish-server/server.js
+++ Week 19/publish-server/server.js
@@ -1,8 +1,12 @@
 const http = require('http')
+const fs = require('fs')
 
 
 http.createServer(function(request, response) {
   console.log(request.headers)
+
+  fs.createWriteStream('../server/publish/index.html')
+
   request.on('data', chunk => {
     console.log(chunk.toString())
   })
